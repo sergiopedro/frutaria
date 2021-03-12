f7c50394b031c1038113971c9d7b44ae5884a1ff
Sergio Pedro <deltabite@msn.com>
2021-03-12 11:55:34 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListaFrutas from './components/ListaFrutas';
+
+function App() {
+  return (
+    <div className="App">
+      <ListaFrutas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Fruta.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { actions } from '../actions/frutas.action';
+
+const Fruta = ({ fruta }) => {
+
+    const dispatch = useDispatch();
+
+    return (
+        <div className="fruta">
+            <ul>
+                <li>
+                    <strong>Fruta: </strong> {fruta.nome}
+                </li>
+                <li>
+                    <strong>Quantidade: </strong> {fruta.quantidade}
+                </li>
+            </ul>
+            <button onClick={() => dispatch(actions.remover(fruta))}>Remover</button>
+        </div>
+    )
+}
+
+export default Fruta;
